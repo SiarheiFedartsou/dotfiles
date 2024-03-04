41f5799d004a458b5c0259f071d37610b9eb168a
Siarhei Fedartsou <siarhei.fedartsou@gmail.com>
2024-03-04 17:27:16 +0100

    Update .wezterm.lua

--- .wezterm.lua
+++ .wezterm.lua
@@ -6,8 +6,12 @@ local config = wezterm.config_builder()
 
 -- This is where you actually apply your config choices
 
+config.window_frame = {
+  font_size = 24.0
+}
+
 -- For example, changing the color scheme:
-config.color_scheme = 'Batman'
+config.color_scheme = 'Catpuccin Mocha'
 config.font = wezterm.font 'Fira Code' --'MesloLGS NF'
 
 -- and finally, return the configuration to wezterm
